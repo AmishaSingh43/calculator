2b01439f793f6f3c693cbe6b537eb1b65940688d
AmishaSingh43 <amishasingh4395@gmail.com>
2024-05-28 13:56:50 +0400

    rspec for string calculator code

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,7 +3,20 @@ RSpec.describe Calculator do
     expect(Calculator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
+
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(Calculator::StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      expect(Calculator::StringCalculator.add("1")).to eq(1)
+      expect(Calculator::StringCalculator.add("5")).to eq(5)
+    end
+    
   end
+
 end
