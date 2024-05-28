ed7bbfc6980d1b2c59a95aea6f61872fbafb28df
AmishaSingh43 <amishasingh4395@gmail.com>
2024-05-28 13:56:05 +0400

    calculate string logic

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,26 @@
 require "calculator/version"
 
 module Calculator
-  class Error < StandardError; end
-  # Your code goes here...
+
+  class StringCalculator
+    def self.add(numbers)
+      return 0 if numbers.empty?
+      
+      delimiter = /,|\n/
+      if numbers.start_with?("//")
+        delimiter_spec, numbers = numbers.split("\n", 2)
+        delimiter = Regexp.escape(delimiter_spec[2..-1])
+      end
+
+      number_list = numbers.split(/#{delimiter}/).map(&:to_i)
+      
+      negative_numbers = number_list.select { |n| n < 0 }
+      unless negative_numbers.empty?
+        raise "negative numbers not allowed: #{negative_numbers.join(', ')}"
+      end
+
+      number_list.sum
+    end
+  end
+
 end
